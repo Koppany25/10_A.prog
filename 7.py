8ae2ab22c830832e29c62f3f6ba4bd35ce9729bc
Koppany25 <galikoppany@gmail.com>
2023-12-18 11:41:38 +0100

    8

--- 7.py
+++ 7.py
@@ -1,32 +1,102 @@
-#Adatok:
-#   Fájlban: Dolgozók nevei
-#               Mellette hogy mennyi palacsintát ettek meg
+# Adatok:
+#   Fájlban:    Dolgozók nevei szerepelnek
+#               Mellette hogy havonta mennyi palacsintát sütöttek meg
 
-#1.Olvasd be a fájlt
+# 1) Olvasd be a fájlt megfelelő adatszerkezetbe!
 
-name = "imput.txt"
-file = open(name, "r")
-file_l = file.readlines()
+filename="input.txt"
+
+file=open(filename, "r")
+file_lista=file.readlines()
 file.close()
 
-name_l = []
-pk_l = []
+name_list=[]
+pancake_list=[]
+for i in range(len(file_lista)):
+    _lista=file_lista[i].strip()
+    _lista_parts=_lista.split()
 
-for i in range(len(file_l)):
-    _lista = file_l[i].strip()
-    print(_lista)
-    _lista_p = _lista.split()
-    name_l.append(_lista_p[0])
+    # így is lehet: _lista_parts=file_lista[i].strip().split()
+    name_list.append(_lista_parts[0])
 
-    _l = []
-    for j in range(1, len(_lista_p)):
-        _l.append(int(_lista_p[j]))
-    pk_l.append(_l)
+    _l=[]
+    for j in range(1,len(_lista_parts)):
+        _l.append(int(_lista_parts[j]))
+    pancake_list.append(_l)
 
-print(name_l)
-print(pk_l)
-for i in range(len(pk_l)):
-    print(len(pk_l[i]))
+print(name_list)
+print(pancake_list)
+for i in range(len(pancake_list)):
+    print(len(pancake_list[i]))
 
-#2. Írd ki az adatokat
-#   "Név\tJanuar\tFebruar\tMarch\tApril\tMay\tAugust\tSeptember\tOctober\tDecember"
+# 2) írd ki az adatokat szépen(!!) az alábbbi fejléccel:
+#       "Név\tJanuary\tFebruary\tMarch\tApril\tMay\tJune\tJuly\tAugust\tSeptember\tOctober\tNovember\tDecember"
+fejlec = ["Név", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+
+ho = 13 #sor
+na = len(name_list) + 1 #oszlop
+lst = [[0]*ho for j in range(na)]
+
+#név
+j = 0
+for i in range(1, na):
+        lst[i][0] = name_list[j]
+        j += 1
+
+
+for x in range(len(lst)):
+    lst[0] = fejlec
+
+
+'''j = 0
+k = 0
+for i in range(1, na):
+    for j in range(1, ho):
+        lst[i][j] = pancake_list[j][k]
+        k += 1
+        j += 1'''
+
+
+for x in lst:
+    for y in x:
+        print("%3s" %(y), end = " ")
+    print("\n")
+
+
+# 3) Add meg, hogy márciusban ki sütötte a legtöbb palacsintát!
+# 4) Add meg, hogy ki sütötte az évben a legkevesebb palacsintát!
+# 5) Add meg, hogy az a személy, akinek B-vel kezdődik a neve, Májusban hány palacsintát sütött.
+# 6) Volt-e olyan személy, akinek az évben változó teljesítménye volt?
+#       Pl: sütött valamennyit, majd a következő hónapban többet, a következőben az előzőnél kevesebbet stb
+#       írd ki a személy nevét!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
